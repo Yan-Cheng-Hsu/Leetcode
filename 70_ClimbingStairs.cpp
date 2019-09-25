c406b468db92a9a69f5ee273541cf3de276e4fe2
Yan-Cheng-Hsu <dragonbily13579@gmail.com>
2019-09-25 23:19:26 +0800

    70_AC

--- 70_ClimbingStairs.cpp
+++ 70_ClimbingStairs.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdio.h>
+#include<vector>
 using namespace std;
 
 
@@ -12,17 +13,33 @@ int Solution::climbStairs(int n)
 {
     if(n==1)return 1;
     else if(n==2)return 2;
-    else return ( climbStairs(n-1) +climbStairs(n-2) );
+    else
+    {
+        int index1 = 0;
+        int index2 = 1;
+        vector<int> step_array;
+        step_array.push_back(1);
+        step_array.push_back(2);
+        for(int i=0;i<(n-2);i++)
+        {
+            int temp = step_array[index1] + step_array[index2];
+            step_array.push_back(temp);
+            index1++;
+            index2++;
+        }
+        return step_array[n-1];          
+    }
 }
 
 
 int main(void)
 {
     Solution s;
-    cout<<"plz input the num of stairs: ";
-    int n;
-    cin>>n;
-    cout<<"Answers: "<<s.climbStairs(n)<<endl;
+    for(int i=0;i<20;i++)
+    {
+        cout<<s.climbStairs(i)<<" ";
+    }
+    cout<<endl;
     system("pause");
     return 0;
 }

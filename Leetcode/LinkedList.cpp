b4b7805f04b7756e3d83ad84be97a3f79c0373fd
Yan-Cheng-Hsu <dragonbily13579@gmail.com>
2021-04-25 23:55:21 +0800

    added 'LinkedList catergory.'

---
+++ Leetcode/LinkedList.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector.h>
+using namespace std;
+
+
+class Solution
+{
+    public:
+        void test();
+
+};
+
+
+void Solution::test()
+{
+    return;
+}
+
+
+
+int main(void)
+{
+    Solution S;
+    S.test();
+    system("pause");
+    return 0;
+}
+
+
+

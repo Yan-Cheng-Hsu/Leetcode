edd0c63c75682b77fac9003b744bb0a1fca42988
Yan-Cheng-Hsu <dragonbily13579@gmail.com>
2021-06-17 20:27:25 +0800

    198 done.

--- Leetcode/DynamicProgramming.cpp
+++ Leetcode/DynamicProgramming.cpp
@@ -36,7 +36,26 @@ int Solution::climbStairs70( int n )
 
 int Solution::rob198( vector<int> &nums )
 {
+    if( nums.size() == 0 ) 
+        return 0;
+    if(  nums.size() == 1 ) 
+        return nums[0];
+    if( nums.size() == 2 ) 
+        return max( nums[0] , nums[1] );
+
+
+    int *loot = new int[nums.size()];
+
+    loot[0] = nums[0];
+    loot[1] = max( nums[0] , nums[1] );
     
+    for(int i=2;i<nums.size();i++)
+        loot[i] = max( loot[i-1] , loot[i-2] + nums[i] );
+    
+
+    return loot[nums.size()-1]; 
+
+
 }
 
 
